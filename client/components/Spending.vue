bb89f163be48229ef7c8134e68323128f4753295
Falestio <falestioltmpt@gmail.com>
2022-01-24 22:06:50 +0700

    added spending components

---
+++ client/components/Spending.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-10/12 mx-auto">
+    <div class="p-8 rounded-md border-2 border-red-100">
+        <div class="flex justify-between">
+            <span class="text-md text-gray-400">12/3/2022</span>
+            <span class="text-md text-red-400 font-bold">- Rp.25.000</span>
+        </div>
+        <div class="mt-4">
+            <h2 class="text-lg font-bold mb-2">Beli jam baru</h2>
+            <p class="text-lg mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis incidunt odio aliquam iure cumque tempore nostrum quos recusandae explicabo ab.</p>
+            <span class="text-md text-red-400">Rp.425.000 -> Rp.400.000</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
