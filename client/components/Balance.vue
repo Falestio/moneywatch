205a1106cc814fc23a201ecc3702254df03ad03e
Falestio <falestioltmpt@gmail.com>
2022-01-24 22:02:44 +0700

    add Balance components and css

---
+++ client/components/Balance.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-10/12 mx-auto">
+      <div class="p-8 rounded-md border-2 border-gray-200">
+          <div class="flex flex-col gap-4 text-center">
+              <h2 class="text-2xl font-bold">Balance</h2>
+              <span class="text-4xl text-blue-500">Rp. 400.000</span>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    
+</style>>
+
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <Tutorial />
+<div class="px-8 md:px-24 lg:px-44 mt-10">  
+  <Balance />
+
+  <div class="flex flex-col gap-8 mt-10">
+    <Spending />
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'Homepage',
 }
 </script>
