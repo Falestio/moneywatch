98b7de61c8ed6e404865bc2ed1bbbee327b6c49c
Falestio <falestioltmpt@gmail.com>
2022-02-05 17:38:21 +0700

    adding file

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,8 @@
 <template>
-<div class="px-8 md:px-24 lg:px-44 mt-10">  
-  <Balance />
-
-  <div class="flex flex-col gap-8 mt-10">
-    <Spending />
+  <div class="px-8 md:px-24 lg:px-44 mt-10">  
+    <Balance />
+    <Form />
   </div>
-</div>
 </template>
 
 <script>
