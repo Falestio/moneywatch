4044d3856f198b0515afe9a655cbf4bfe7cc6b41
Falestio <falestioltmpt@gmail.com>
2022-02-05 17:32:59 +0700

    adding spending and form vue

---
+++ client/components/Form.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="w-10/12 mx-auto mt-4">
+        <div class="flex justify-between">
+            <button class="btn bg-red-400 text-white active:bg-red-600" @click="toggleSpendingForm">Spending</button>
+            <button class="btn bg-blue-500 text-white active:bg-blue-700" @click="toggleDepositForm">Deposit</button>
+        </div>
+
+        <form action="post" class="flex flex-col gap-4 mt-8" v-if="showDepositForm">
+            <h3 class="text-xl">Deposit</h3>
+
+            <label for="title" class="font-md font-bold">Title</label>
+            <input name="title" type="text" class="border-2 p-4" id="title">
+
+            <label for="detail" class="font-md font-bold">Detail</label>
+            <textarea name="detail" id="detail" cols="30" rows="10" class="border-2 p-4"></textarea>
+            
+            <label for="amount" class="font-md font-bold">Amount</label>
+            <input name="amount" type="number" class="border-2 p-4" id="amount">
+            
+            <button class="btn bg-blue-500 text-white">Submit</button>
+        </form>
+
+
+        <form action="post" class="flex flex-col gap-4 mt-8" v-if="showSpendingForm">
+            <h3 class="text-xl">Spending</h3>
+
+            <label for="title" class="font-md font-bold">Title</label>
+            <input name="title" type="text" class="border-2 p-4" id="title">
+
+            <label for="detail" class="font-md font-bold">Detail</label>
+            <textarea name="detail" id="detail" cols="30" rows="10" class="border-2 p-4"></textarea>
+            
+            <label for="amount" class="font-md font-bold">Amount</label>
+            <input name="amount" type="number" class="border-2 p-4" id="amount">
+            
+            <button class="btn bg-blue-500 text-white">Submit</button>
+        </form>
+
+        <div class="mt-10 w-full">
+            <Spending :spendings="spendings" class="flex flex-col gap-4" />
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            showDepositForm: false,
+            showSpendingForm: false,
+            spending: {
+                title: '',
+                detail: '',
+                amount: 0
+            },
+            spendings: [
+                {
+                    title: "Alfamart",
+                    detail: "membeli makanan dan jajanan serta kopi",
+                    amount: 30000
+                },
+                {
+                    title: "Indomaret",
+                    detail: "membeli makanan dan jajanan serta kopi",
+                    amount: 50000
+                },
+                {
+                    title: "Membeli Celana Jeans",
+                    detail: "Celana jeans baru promo dari transmart",
+                    amount: 170000
+                }
+            ]
+        }
+    },
+
+    methods: {
+        toggleDepositForm(){
+            this.showDepositForm = !this.showDepositForm
+        },
+        toggleSpendingForm(){
+            this.showSpendingForm = !this.showSpendingForm
+        },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- client/components/Spending.vue
+++ client/components/Spending.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="w-10/12 mx-auto">
+<div>
+  <div class="mx-auto w-full" v-for="spending in spendings" :key="spending.title">
     <div class="p-8 rounded-md border-2 border-red-100">
         <div class="flex justify-between">
             <span class="text-md text-gray-400">12/3/2022</span>
-            <span class="text-md text-red-400 font-bold">- Rp.25.000</span>
+            <span class="text-md text-red-400 font-bold">- Rp.{{spending.amount}}</span>
         </div>
         <div class="mt-4">
-            <h2 class="text-lg font-bold mb-2">Beli jam baru</h2>
-            <p class="text-lg mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis incidunt odio aliquam iure cumque tempore nostrum quos recusandae explicabo ab.</p>
+            <h2 class="text-lg font-bold mb-2">{{ spending.title }}</h2>
+            <p class="text-lg mb-2">{{ spending.detail }}</p>
             <span class="text-md text-red-400">Rp.425.000 -> Rp.400.000</span>
         </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    spendings: { type: Array, required: true}
+  }
 }
 </script>
 
